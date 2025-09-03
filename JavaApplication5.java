e457af8e6febc9e8eab3dc4cf073071c852d0f57
juju <aj.ncorrea@gmail.com>
2025-09-03 10:14:59 -0300

    Add files via upload

---
+++ JavaApplication5.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication5;
+
+/**
+ *
+ * @author ANNAJULIADENORONHACO
+ */
+public class JavaApplication5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    se (nota >= 6)
+        escreva ("aprovado")
+}
+if (nota >= 6){
+    System.out.println("aprovado");
+}
---
+++ JavaApplication6.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication6;
+
+/**
+ *
+ * @author ANNAJULIADENORONHACO
+ */
+public class JavaApplication6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    algoritmo par
+            inicio
+                    inteiro num
+                            escreva ("digite um numero inteiro")
+                            keia (num)
+                            se (num%2==0){
+                                escreva ("0 numero :" + num + "e par")
+                            }
+                            fim
+}
+import java.util.Scanner;
+public class Exemplo2 {
+    public static void main(String[]args){
+        Scanner SC = new Scanner(System.in);
+        System.out.println("maior de idade';");
+    }else {
+    System.out.println("menor de idade");
+}
+    sc.close();
+}
