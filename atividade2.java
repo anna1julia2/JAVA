f896dfd1f7d2b93022152797638346e945601635
juju <aj.ncorrea@gmail.com>
2025-09-03 10:40:17 -0300

    Add files via upload

---
+++ atividade2.java
@@ -0,0 +1,35 @@
+public class atividade2 {
+    
+}
+//java
+Scanner sc = new Scanner (System.in);
+System.out.println("digite seu peso: ");
+float peso = sc.nextFloat();
+System.out.println("digite sua altura: ");
+float altura = sc.nextFloat();
+float imc = peso / (altura+altura);
+System.out.println("o seu imc é: " + imc + "kg/m");
+sc.close();
+
+algoritmo media
+inicio
+   real peso, altura, imc
+escreva ("digite o seu peso")
+leia (peso)
+escreva ("digite sua altura")
+leia (altura)
+leia (altura
+imc=peso/(altura*altura)
+escreva ("seu imc é: ", imc, "kg/m")
+fimalgoritmo
+#atividade.py
+peso = float(input("digite seu peso: "))
+altura = float(input("digite sua altura: "))
+imc = peso / (altura * altura)
+print("seu imc é: ", imc, "kg/m")
+#atividade.js
+let peso = parseFloat(prompt("digite seu peso: "));
+let altura = parseFloat(prompt("digite sua altura: "));
+let imc = peso / (altura * altura);
+console.log("seu imc é: " + imc + "kg/m");
+alert("seu imc é: " + imc + "kg/m");
