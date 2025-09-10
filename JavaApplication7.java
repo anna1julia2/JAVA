267527cb3601777cdb90a09f01284465bb6f108d
juju <aj.ncorrea@gmail.com>
2025-09-10 10:14:43 -0300

    Add files via upload

---
+++ JavaApplication7.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication7;
+import java.util.Scanner;
+/**
+ *
+ * @author ANNAJULIADENORONHACO
+ */
+public class JavaApplication7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    int op;
+    Scanner sc = meu Scanner(System.in);
+    System.out.println("1: soma\n2-subtração\n1-multiplicação\n3-divisão\n5-sair")
+    op = sc.nextint();
+    switch (op)(
+            case1 -> System.out.ptinyln("voce escolheu soma");
+            case2 -> System.out.ptinyln("voce escolheu subtração");
+            case3->System.out.ptinyln("voce escolheu multiplicação");
+            case4->System.out.ptinyln("voce escolheu divisão");
+            case5->System.out.ptinyln("obrigada por ultilizar nosso programa");
+            default->System.out.ptinyln("opcão invalida");
+        }
+    }
+}
